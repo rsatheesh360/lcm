0b00e910ae056ce159bba7c6038e554b638ccab1
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-04-04 12:13:31 +0530

    Update and rename .gitignore to lcm.py

---
+++ .gitignore/lcm.py
@@ -0,0 +1,12 @@
+x,y=map(int,input().split(' '))
+if x > y:
+    greater = x
+else:
+    greater = y
+while(True):
+    if((greater % x == 0) and (greater % y == 0)):
+        lcm = greater
+        break
+    greater += 1
+print lcm
+ print(n)
